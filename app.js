7b0f7bff7c2783997ea92b9d1652e3a96bf3799a
Scott Juneau <dsjuneau@gmail.com>
2020-10-20 18:34:39 -0500

    Closer to working

--- app.js
+++ app.js
@@ -2,6 +2,25 @@ const gen = document.getElementById("btn-1");
 const iter = document.getElementById("txt-1");
 const flips = document.getElementById("txt-2");
 const pro1 = document.getElementById("progress-1");
+const timer = document.getElementById("timer");
+const time = Date.now();
+
+const pmf = (nof, its) => {
+  const time1 = Date.now();
+  const pmfArray = [];
+  pmfArray.length = nof;
+  pmfArray.fill(0);
+  for (let i = 0; i < its; i++) {
+    let count = 0;
+    for (let j = 0; j < nof; j++) {
+      let a = Math.floor(2 * Math.random());
+      count = count + a;
+    }
+    pmfArray[count] = pmfArray[count] + 1;
+  }
+
+  console.log("running it");
+};
 
 const flipper = (e) => {
   // Looks like disabling a button has the same effect and removing the event listener
@@ -9,12 +28,15 @@ const flipper = (e) => {
   gen.disabled = true;
   pro1.style = "display: visible";
   pro1.value = 0;
-  //   gen.removeEventListener("click", flipper);
+  //  gen.removeEventListener("click", flipper);
   const iterations = Number(iter.value);
   const numOfFlips = Number(flips.value);
-
-  console.log(iterations);
-  console.log(numOfFlips);
+  pmf(numOfFlips, iterations);
 };
+
+// This adds a seconds
+setInterval(() => {
+  timer.innerHTML = "Seconds: " + ((Date.now() - time) / 1000).toString();
+}, 40);
 
 gen.addEventListener("click", flipper);
